15a1a5e99bebcdca6baed5350a8276cf122570df
QuentinAyers1917 <rkowert@posteo.de>
2024-07-09 20:08:11 +0200

    improve comments section

--- static/js/replyToComment.js
+++ static/js/replyToComment.js
@@ -1,17 +1,19 @@
-const replyBtn = document.querySelector("#reply-btn");
-const comment_id = replyBtn.dataset.commentId;
-const replyTextareaDiv = document.querySelector(`#reply-to-${comment_id}`);
-replyBtn.addEventListener("click", () => {
-    console.log(replyTextareaDiv);
-    if (replyTextareaDiv.classList.contains("hidden")) {
-        replyTextareaDiv.classList.remove("hidden");
-    } else {
-        replyTextareaDiv.classList.add("hidden");
-    }
-})
+const replyButtons = document.querySelectorAll("#reply-btn");
+for (const btn of replyButtons) {
+    btn.addEventListener("click", () => {
+        const comment_id = btn.dataset.commentId;
+        const replies = document.querySelector(`#replies-${comment_id}`);
+        if (replies.classList.contains("hidden")) {
+            replies.classList.remove("hidden");
+        } else {
+            replies.classList.add("hidden");
+        }
+    })
+}
 
 function replyToComment(comment_id) {
     const replyText = document.querySelector(`#textarea-${comment_id}`).value;
+    const replies = document.querySelector(`#replies-inner-${comment_id}`);
 
     fetch(`/reply-to-comment/${comment_id}`, {
         method: "POST",
@@ -28,8 +30,8 @@ function replyToComment(comment_id) {
     repliesCount.innerText = parseInt(repliesCount.innerText) + 1;
     const newReply = document.querySelector("#template-reply");
     newReply.children[1].children[1].innerText = replyText;
-    const replies = document.querySelector("#replies");
     replies.appendChild(newReply);
-    newReply.classList.remove("hidden")
-    replyTextareaDiv.children[0].value = "";
+    newReply.classList.remove("hidden");
+    const replyTextarea = document.querySelector(`#textarea-${comment_id}`);
+    replyTextarea.value = "";
 }
