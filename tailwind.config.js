1d48d6e3a6cc8a74fb0ceb15d6aaf888f2d5500e
QuentinAyers1917 <rkowert@posteo.de>
2024-12-01 17:11:56 +0100

    make blogpost cards on index page repsonsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
   content: ["./templates/*.html"],
   theme: {
     container: {
-      padding: '15rem',
+      padding: {
+        DEFAULT: "2rem",
+        sm: '2.5vw',
+        lg: '5vw',
+        xl: '10vw',
+        '2xl': '12.5vw',
+      },
+      screens: {
+        sm: '900px',
+        lg: '1080px',
+        xl: '1380px',
+        '2xl': '1600px',
+      }
     },
     extend: {},
   },
