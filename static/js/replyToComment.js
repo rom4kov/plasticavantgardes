f89a02c5533b3ad2a7e3ae66db443cd62febbd66
QuentinAyers1917 <rkowert@posteo.de>
2024-12-08 18:42:35 +0100

    solve superfluous space below bottom of blogpost on posts page

--- static/js/replyToComment.js
+++ static/js/replyToComment.js
@@ -1,37 +1,39 @@
 const replyButtons = document.querySelectorAll("#reply-btn");
 for (const btn of replyButtons) {
-    btn.addEventListener("click", () => {
-        const comment_id = btn.dataset.commentId;
-        const replies = document.querySelector(`#replies-${comment_id}`);
-        if (replies.classList.contains("hidden")) {
-            replies.classList.remove("hidden");
-        } else {
-            replies.classList.add("hidden");
-        }
-    })
+  btn.addEventListener("click", () => {
+    const comment_id = btn.dataset.commentId;
+    const replies = document.querySelector(`#replies-${comment_id}`);
+    if (replies.classList.contains("hidden")) {
+      replies.classList.add("h-fit");
+      replies.classList.remove("hidden");
+    } else {
+      replies.classList.add("hidden");
+      replies.classList.remove("h-fit");
+    }
+  });
 }
 
 function replyToComment(comment_id) {
-    const replyText = document.querySelector(`#textarea-${comment_id}`).value;
-    const replies = document.querySelector(`#replies-inner-${comment_id}`);
+  const replyText = document.querySelector(`#textarea-${comment_id}`).value;
+  const replies = document.querySelector(`#replies-inner-${comment_id}`);
 
-    fetch(`/reply-to-comment/${comment_id}`, {
-        method: "POST",
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            text: replyText
-        })
-    })
+  fetch(`/reply-to-comment/${comment_id}`, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      text: replyText,
+    }),
+  });
 
-    const repliesCount = document.querySelector("#replies-count");
-    repliesCount.innerText = parseInt(repliesCount.innerText) + 1;
-    const newReply = document.querySelector("#template-reply");
-    newReply.children[1].children[1].innerText = replyText;
-    replies.appendChild(newReply);
-    newReply.classList.remove("hidden");
-    const replyTextarea = document.querySelector(`#textarea-${comment_id}`);
-    replyTextarea.value = "";
+  const repliesCount = document.querySelector("#replies-count");
+  repliesCount.innerText = parseInt(repliesCount.innerText) + 1;
+  const newReply = document.querySelector("#template-reply");
+  newReply.children[1].children[1].innerText = replyText;
+  replies.appendChild(newReply);
+  newReply.classList.remove("hidden");
+  const replyTextarea = document.querySelector(`#textarea-${comment_id}`);
+  replyTextarea.value = "";
 }
